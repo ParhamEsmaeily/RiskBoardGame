efd5e98110e1c8259264ea34d28a9ecbbce5f12c
maxime334 <121265989+maxime334@users.noreply.github.com>
2023-11-07 21:52:43 +0000

    Cards and Player changes.
    Pushed some important changes to Cards and Player class.

--- Cards/Cards.h
+++ Cards/Cards.h
@@ -1,59 +1,76 @@
 #pragma once
 #include <algorithm>
+#include <iostream>
 #include <memory>
 #include <ostream>
+#include <random>
 #include <string>
 #include <vector>
-
-void test_cards(int no_cards = 15);
+// Created by Maxime Landry (maxime334) on 23-10-14
 
 /*
-  Five card_type available:
-  0- bomb,
-  1- reinforcement,
-  2- blockade,
-  3- airlift,
-  4- diplomacy.
-*/
-enum card_type
-{
+  Five possible type of cards:
   bomb,
   reinforcement,
   blockade,
   airlift,
-  diplomacy
-};
+  diplomacy.
+*/
+enum class Type : char { bomb, reinforcement, blockade, airlift, diplomacy };
 
-namespace cd
-{
-  /*
-  Maps enum value to a string value. Enum value directly does not give a string
-  value.
-  */
-  std::string type_map(const int);
+namespace cd {
+/*
+Maps class enum value to a string value.
+Used internally inside the stream insertion operator overload.
+*/
+const std::string map(const Type);
 } // namespace cd
 
+void test_cards(int);
+
 class Card;
+class Buffer;
 class Deck;
 class Hand;
-// Forward-Declaration.
+// Forward-Declaration.`
 
+// Stream insertion overload.
+std::ostream &operator<<(std::ostream &, const Type &);
+
+std::ostream &operator<<(std::ostream &, const Card &);
+
+std::ostream &operator<<(std::ostream &, const Buffer &);
+
+std::ostream &operator<<(std::ostream &, const Hand &);
+
+std::ostream &operator<<(std::ostream &, const Deck &);
+
+bool operator==(const Card &, const Card &) noexcept;
+
+// Unique_ptr to card.
 using card_ptr = std::unique_ptr<Card>;
 // Vector holding unique_ptr to Cards.
 using card_vector = std::vector<card_ptr>;
 
 /*
     Warzone card.
+    To declare a type for the card, use Type::type where type is to be
+   chosen.
 */
-class Card
-{
+class Card {
 public:
   // Type of the card. 5 types available.
-  const card_type type;
+  // Type::type to be used. Check enum class for further details.
+  const Type m_type;
 
-  Card(const card_type);
+  Card() = delete; // Assures class is called w/ initializer.
+  // Takes rvalue as argument. Better optimization.
+  Card(Type &&);
+  Card(const Type &);
+  // Move constructor.
+  Card(Card &&);
   Card(const Card &);
-  // Destructor does not seem necessary for this class.
+  ~Card() = default;
 
   /*
     Creates an order an adds it to the player's list of orders
@@ -64,27 +81,25 @@ public:
   /*
     Returns string type of the card.
   */
-  const std::string value() const noexcept;
+  const std::string type() const noexcept;
 
-  friend std::ostream &operator<<(std::ostream &os, const Card &c)
-  {
-    // Mapping is made from enum index to the proper string type.
-    os << "Card is of type: " << cd::type_map(c.type);
-    return os;
-  }
+  /*
+    Card class should not be assignable, and a new Card should just be created.
+    Compile-time error will occur, so nothing happens in the dark.
+  */
+  Card &operator=(const Card &) = delete;
 };
 
 /*
   Base class when holding cards is necessary.
   Container of cards.
 */
-class Buffer
-{
-protected:
-  card_vector buffer;
+class Buffer {
+public:
+  card_vector m_buffer;
 
   /*
-    Manages the removal of the card from the buffer.
+    Manages the removal of the card from the m_buffer.
     Returns a unique_ptr to the card.
   */
   card_ptr remove(const int index);
@@ -92,24 +107,26 @@ protected:
 public:
   Buffer();
   Buffer(const Buffer &);
+  ~Buffer() = default;
 
   /*
-    Insert a card by reference inside the deck.
+    Insert a card inside the m_buffer.
+    Passed by value.
   */
-  void insert(const Card &) noexcept;
+  void insert(const Card) noexcept;
 
   /*
     Inserts a card of the proper type inside the player's Hand.
     Creates a new card.
   */
-  void insert(const card_type) noexcept;
+  void insert(const Type) noexcept;
 
   /*
     Returns a vector of all the cards, as types, contained inside the hand.
     The vector can be implicitely converted to a vector of cards inside a
-    foreach loop.
+    for-each loop.
   */
-  std::vector<card_type> show_cards() const noexcept;
+  std::vector<Type> show_cards() const noexcept;
 
   /*
     Returns number of cards left inside the player's hand.
@@ -117,39 +134,53 @@ public:
   int size() const noexcept;
 
   /*
-    Removes every card inside the buffer. Clears it.
+    Removes every card inside the m_buffer. Clears it.
   */
   void clear() noexcept;
 
   /*
-    Deep copy of the buffer is made.
+    Deep copy of the m_buffer is made.
   */
-  void operator=(const Buffer &) noexcept;
+  Buffer &operator=(const Buffer &) noexcept;
 };
 
 /*
   Finite collection of Warzone cards.
 */
-class Hand : public Buffer
-{
+class Hand : public Buffer {
 public:
+  Hand();
+  Hand(const Hand &);
+  ~Hand() = default;
   /*
     The card with desired type will be played, and removed from the player's
     hands. The card is then placed back in the deck.
-    Returns false if the card is not in the hand of the player.
+    Returns false if the card is not in the player's hand.
   */
-  bool play(const card_type, Deck &);
+  bool play(const Type, Deck &);
+
+  /*
+    Deep copy of the Hand is made.
+  */
+  Hand &operator=(const Hand &) noexcept;
 };
 
 /*
   Contains finite collection of Warzone cards.
 */
-class Deck : public Buffer
-{
+class Deck : public Buffer {
 public:
+  Deck();
+  Deck(const Deck &);
+  ~Deck() = default;
   /*
     Draw a card at random from the cards remaining in the deck.
     Place it in the their hand of cards.
    */
   void draw(Hand &);
+
+  /*
+    Deep copy of the Deck is made.
+  */
+  Deck &operator=(const Deck &) noexcept;
 };
--- Cards/CardsDriver.cpp
+++ Cards/CardsDriver.cpp
@@ -6,22 +6,18 @@
   Creates a deck of cards, then creates a hand by drawing repeatedly from the
   deck.
 */
-void test_cards(int no_cards)
-{
+void test_cards(int no_cards) {
   Deck deck;
-  for (int i = 0; i < no_cards; i++)
-  {
-    deck.insert(card_type(i % 5)); // Kind of random insertion of cards.
+  for (int i = 0; i < no_cards; i++) {
+    deck.insert(Type(i % 5)); // Kind of random insertion of cards.
   }
 
   Hand hand;
-  for (int i = 0; i < no_cards; i++)
-  {
+  for (int i = 0; i < no_cards; i++) {
     deck.draw(hand);
   }
 
-  for (int i = 0; i < no_cards; i++)
-  {
-    hand.play(card_type(i % 5), deck);
+  for (int i = 0; i < no_cards; i++) {
+    hand.play(Type(i % 5), deck);
   }
 }
