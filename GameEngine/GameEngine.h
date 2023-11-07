dd120eaf29aa21dfd475a13011434cf958d9749a
maxime334 <121265989+maxime334@users.noreply.github.com>
2023-11-07 22:14:20 +0000

    Changes added to the updated GameEngine class.
    Changes are still missing in both GameEngine and CommandProcessor.

--- GameEngine/GameEngine.h
+++ GameEngine/GameEngine.h
@@ -1,8 +1,8 @@
 #pragma once
-
+#include "../LoggingObserver/LoggingObserver.h"
 #include <iostream>
-#include <string>
 #include <memory>
+#include <string>
 #include <vector>
 
 using std::ostream;
@@ -14,48 +14,54 @@ void testGameStates();
 
 class State; // Forward declaration
 
-class Command
-{
+class Command : private ILoggable, private Subject {
+private:
+  /*
+    Records effect of the command into the command object.
+  */
+  std::string m_effect;
+
 public:
-    shared_ptr<string> action;
-    shared_ptr<State> nextState;
-    vector<string> validStates;
-    shared_ptr<string> effect;
+  shared_ptr<string> action;
+  shared_ptr<State> nextState;
 
-    Command() = default;
-    Command(string action, shared_ptr<State> nextState);
+  Command() = default;
+  Command(string action, shared_ptr<State> nextState);
+  Command(Command const &other);
+  Command &operator=(const Command &other);
 
-    Command(Command const &other);
-    Command &operator=(const Command &other);
-    friend ostream &operator<<(ostream &os, const Command &command);
+  void saveEffect(std::string);
+  std::string stringToLog() const override;
 
-    void saveEffect(const std::string &effect);
+  friend ostream &operator<<(ostream &os, const Command &command);
 };
 
-class State
-{
+class State {
 public:
-    shared_ptr<string> phase;
-    vector<shared_ptr<Command>> commands;
-    explicit State(string phase);
+  shared_ptr<string> phase;
+  vector<shared_ptr<Command>> commands;
+  explicit State(string phase);
 
-    State(State const &other);
-    State &operator=(const State &other);
-    friend ostream &operator<<(ostream &os, const State &state);
+  State(State const &other);
+  State &operator=(const State &other);
+  friend ostream &operator<<(ostream &os, const State &state);
 };
 
-class GameEngine
-{
-    shared_ptr<State> currState;
-    void initGame();
+class GameEngine : private ILoggable, private Subject {
+  shared_ptr<State> currState;
+  void initGame();
 
 public:
-    string getCurrCommandsList();
-    string executeCommand(string input);
-    string getPhase();
+  string getCurrCommandsList();
+  string executeCommand(string input);
+  string getPhase();
+  /*
+    Returns the phase of the current state.
+  */
+  std::string stringToLog() const override;
 
-    GameEngine();
-    GameEngine(GameEngine const &other);
-    GameEngine &operator=(const GameEngine &other);
-    friend ostream &operator<<(ostream &os, const GameEngine &gameEngine);
+  GameEngine();
+  GameEngine(GameEngine const &other);
+  GameEngine &operator=(const GameEngine &other);
+  friend ostream &operator<<(ostream &os, const GameEngine &gameEngine);
 };
